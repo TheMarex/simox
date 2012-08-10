f8de9427228b3f33582f56905647a8c9b1205554
vahrenkamp <vahrenkamp@042f3d55-54a8-47e9-b7fb-15903f145c44>
2012-08-10 14:06:39 +0000

    Boost 1.50 fix
    git-svn-id: https://svn.code.sf.net/p/simox/code/trunk@306 042f3d55-54a8-47e9-b7fb-15903f145c44

--- VirtualRobot/XML/BaseIO.cpp
+++ VirtualRobot/XML/BaseIO.cpp
@@ -583,7 +583,7 @@ void BaseIO::makeRelativePath( const std::string &basePath, std::string &filenam
 	std::string res = diffpath.string();
 	if (found && origPath.extension()!="")
 	{
-#if (BOOST_VERSION>105000)
+#if (BOOST_VERSION>=105000)
 		std::string ext = origPath.extension().generic_string().c_str(); // should work with with V3 and V2
 #else
 		std::string ext = origPath.extension().c_str(); // should work with with V3 and V2

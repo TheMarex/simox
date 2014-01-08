d6ec7a3f1416977b37f008fcdc3ad9a6b21172c6
vwitten <vwitten@042f3d55-54a8-47e9-b7fb-15903f145c44>
2014-01-08 14:44:22 +0000

    added support for collada
    git-svn-id: https://svn.code.sf.net/p/simox/code/trunk@463 042f3d55-54a8-47e9-b7fb-15903f145c44

--- VirtualRobot/CMakeModules/VirtualRobotConfigDependencies.cmake
+++ VirtualRobot/CMakeModules/VirtualRobotConfigDependencies.cmake
@@ -4,16 +4,16 @@
     SET (VirtualRobot_EXTERNAL_LIBRARY_DIRS "")
     SET (VirtualRobot_EXTERNAL_LIBRARY_FLAGS "")
     SET (VirtualRobot_EXTERNAL_LIBRARY_CMAKE_INCLUDE "")
-	
+
 	############################# SETUP MODULES #############################
 	MESSAGE (STATUS "module path: "  ${CMAKE_MODULE_PATH})
-	
+
 	#### Eigen
 	FIND_PACKAGE (Eigen3 REQUIRED)
 	if (Eigen3_FOUND)
 	    SET (VirtualRobot_EXTERNAL_INCLUDE_DIRS ${VirtualRobot_EXTERNAL_INCLUDE_DIRS} ${Eigen3_INCLUDE_DIR})
 	endif (Eigen3_FOUND)
-	
+
 	#### BOOST
 	FIND_PACKAGE(Boost 1.42.0 COMPONENTS filesystem system unit_test_framework program_options thread REQUIRED)
 	if (Boost_FOUND)
@@ -26,10 +26,10 @@
     else (Boost_FOUND)
 	    MESSAGE ("!! Could not find Boost !!")
 	endif (Boost_FOUND)
-	
-	#### QT 
+
+	#### QT
 	# QT_QMAKE_EXECUTABLE is the only relieable way of setting the qt4 path!
-	# convert env var to cmake define	
+	# convert env var to cmake define
 	IF(NOT "$ENV{QT_QMAKE_EXECUTABLE}" STREQUAL "")
 	    MESSAGE (STATUS "USING QT-PATH from environment variable QT_QMAKE_EXECUTABLE: $ENV{QT_QMAKE_EXECUTABLE}")
 	    file(TO_CMAKE_PATH "$ENV{QT_QMAKE_EXECUTABLE}" QT_QMAKE_EXECUTABLE)
@@ -42,14 +42,15 @@
 	SET (VirtualRobot_VISUALIZATION_LIBS "")
 	SET (VirtualRobot_VISUALIZATION_INCLUDE_PATHS "")
 	SET (VirtualRobot_VISUALIZATION_COMPILE_FLAGS "")
-	
+
 	OPTION(VirtualRobot_USE_COIN_VISUALIZATION "Use Coin3D for visualization" ON)
 	OPTION(VirtualRobot_USE_OPENSCENEGRAPH_VISUALIZATION "Use OpenSceneGraph for visualization" OFF)
-	
-	
+	OPTION(VirtualRobot_USE_COLLADA "Enable the loading of robots from collada files" OFF)
+
+
 	if (VirtualRobot_USE_COIN_VISUALIZATION)
 	    MESSAGE(STATUS "Searching Coin3D, Qt and SoQt...")
-	    
+
 	    ##### Coin3D
 		FIND_PACKAGE(Coin3D REQUIRED)
 		if (COIN3D_FOUND)
@@ -57,7 +58,7 @@
 			##INCLUDE_DIRECTORIES(${COIN3D_INCLUDE_DIRS})
 			##ADD_DEFINITIONS(-DCOIN_DLL)
 		endif (COIN3D_FOUND)
-		
+
 
 		if ( QT_FOUND )
 			MESSAGE (STATUS "Found Qt4: " ${QT_INCLUDE_DIR})
@@ -68,7 +69,7 @@
 			SET (VirtualRobot_EXTERNAL_LIBRARIES ${VirtualRobot_EXTERNAL_LIBRARIES} ${QT_LIBRARIES})
 
 			#MESSAGE(STATUS "QT_LIBRARIES: " ${QT_LIBRARIES})
-	
+
 			#### SoQt
 			# This will set SoQt_INCLUDE_DIRS and SoQt_LIBRARIES
 			FIND_PACKAGE(SoQt)
@@ -81,27 +82,27 @@
 		else ( QT_FOUND )
 			MESSAGE (STATUS "Did not found Qt. Disabling Qt/SoQt support.")
 		endif ( QT_FOUND )
-		
+
 		if (QT_FOUND AND SOQT_FOUND AND COIN3D_FOUND)
 		    MESSAGE (STATUS "Enabling Coin3D/Qt/SoQt support")
-		    MESSAGE (STATUS "By using the Coin3D library, the license of Simox is not LGPL any more. The license must be GPL, since Coin3D is a GPL library. If you want to use Simox under LGPL you must disable Coin3D support!") 
+		    MESSAGE (STATUS "By using the Coin3D library, the license of Simox is not LGPL any more. The license must be GPL, since Coin3D is a GPL library. If you want to use Simox under LGPL you must disable Coin3D support!")
 			SET (VirtualRobot_VISUALIZATION TRUE)
         	SET (VirtualRobot_VISUALIZATION_LIBS ${QT_LIBRARIES} ${COIN3D_LIBRARIES} ${SoQt_LIBRARIES} )
         	SET (VirtualRobot_VISUALIZATION_INCLUDE_PATHS ${QT_INCLUDE_DIR} ${SoQt_INCLUDE_DIRS} ${COIN3D_INCLUDE_DIRS} )
         	SET (VirtualRobot_VISUALIZATION_COMPILE_FLAGS " -DCOIN_DLL -DSOQT_DLL ")
 		endif()
-		
+
 	elseif (VirtualRobot_USE_OPENSCENEGRAPH_VISUALIZATION)
-	
+
 	    MESSAGE(STATUS "Searching OSG and Qt...")
-	    
+
 	    FIND_PACKAGE(OpenSceneGraph REQUIRED osgViewer osgUtil osgDB osgGA)
-		
+
 		if (OPENSCENEGRAPH_FOUND)
 		    MESSAGE (STATUS "Found OpenSceneGraph:" ${OPENSCENEGRAPH_INCLUDE_DIRS})
 			##INCLUDE_DIRECTORIES(${OPENSCENEGRAPH_INCLUDE_DIRS})
 		endif (OPENSCENEGRAPH_FOUND)
-		
+
 		if ( QT_FOUND )
 			MESSAGE (STATUS "Found Qt4: " ${QT_INCLUDE_DIR})
 			include(${QT_USE_FILE})
@@ -109,7 +110,7 @@
 		else ( QT_FOUND )
 			MESSAGE (STATUS "Did not found Qt. Disabling Qt/OSG support.")
 		endif ( QT_FOUND )
-		
+
 		if (QT_FOUND AND OPENSCENEGRAPH_FOUND)
 		    MESSAGE (STATUS "Enabling OSG/Qt support")
 		    ### a little hack is needed here since osgQt is not supported in the FindOSG script
@@ -132,11 +133,30 @@
         	SET (VirtualRobot_VISUALIZATION_INCLUDE_PATHS ${OPENSCENEGRAPH_INCLUDE_DIRS} )
         	SET (VirtualRobot_VISUALIZATION_COMPILE_FLAGS "")
 		endif()
-		
+
 	else()
 	    MESSAGE(STATUS "Visualization disabled")
 	endif()
-	
+
+	if (VirtualRobot_USE_COLLADA)
+	    MESSAGE(STATUS "Searching for Collada...")
+
+    	FIND_PACKAGE(COLLADA_DOM REQUIRED 2.4)
+
+    	IF(COLLADA_DOM_FOUND)
+        	MESSAGE (STATUS "Found Collada")
+        	MESSAGE (STATUS "Collada COLLADA_DOM_ROOT_DIR : ${COLLADA_DOM_ROOT_DIR}")
+	        MESSAGE (STATUS "Collada Include DIRS: ${COLLADA_DOM_INCLUDE_DIRS}")
+	        MESSAGE (STATUS "Collada Libs: ${COLLADA_DOM_LIBRARIES}")
+	        MESSAGE (STATUS "Collada COLLADA_DOM_LIBRARY_DIRS: ${COLLADA_DOM_LIBRARY_DIRS}")
+
+	        FIND_LIBRARY(COLLADA_LIBRARY ${COLLADA_DOM_LIBRARIES} ${COLLADA_DOM_LIBRARY_DIRS})
+	        MESSAGE (STATUS "Collada Full Collada lib: ${COLLADA_LIBRARY}")
+
+	        include_directories(${COLLADA_DOM_INCLUDE_DIRS})
+	    ENDIF()
+	endif()
+
     SET (VirtualRobot_EXTERNAL_INCLUDE_DIRS ${VirtualRobot_EXTERNAL_INCLUDE_DIRS} ${VirtualRobot_VISUALIZATION_INCLUDE_PATHS})
     SET (VirtualRobot_EXTERNAL_LIBRARIES ${VirtualRobot_EXTERNAL_LIBRARIES} ${VirtualRobot_VISUALIZATION_LIBS})
     SET (VirtualRobot_EXTERNAL_LIBRARY_FLAGS "${VirtualRobot_EXTERNAL_LIBRARY_FLAGS} ${VirtualRobot_VISUALIZATION_COMPILE_FLAGS}")
--- config.cmake
+++ config.cmake
@@ -3,14 +3,14 @@ IF (NOT Simox_CONFIGURED)
 
 	# defines Simox_CONFIGURED variable which indicates that this config file has already been included
 	SET(Simox_CONFIGURED TRUE)
-		
+
 	# Set up for debug build
 	IF(NOT CMAKE_BUILD_TYPE)
 	  SET(CMAKE_BUILD_TYPE Debug CACHE STRING
 	      "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
 	      FORCE)
 	ENDIF(NOT CMAKE_BUILD_TYPE)
-	
+
 	############################# VERSION #################################
 	set(Simox_MAJOR_VERSION 2)
 	set(Simox_MINOR_VERSION 2)
@@ -19,26 +19,26 @@ IF (NOT Simox_CONFIGURED)
     ${Simox_MAJOR_VERSION}.${Simox_MINOR_VERSION}.${Simox_PATCH_VERSION})
 
 	MESSAGE (STATUS " ** Simox version: ${Simox_VERSION}")
-	
+
 	############################# SETUP PATHS #############################
 	SET(Simox_BUILD_DIRECTORY ${CMAKE_BINARY_DIR})
 	SET(BIN_DIR bin)
 	SET(LIB_DIR lib)
-	
+
 	SET(Simox_LIB_DIR ${Simox_BUILD_DIRECTORY}/${LIB_DIR})
 	SET(Simox_BIN_DIR ${Simox_BUILD_DIRECTORY}/${BIN_DIR})
-	
+
 	MESSAGE (STATUS "** SIMOX LIB DIR: ${Simox_LIB_DIR}")
 	MESSAGE (STATUS "** SIMOX BIN DIR: ${Simox_BIN_DIR}")
-	
+
 	SET(Simox_INSTALL_LIB_DIR ${CMAKE_INSTALL_PREFIX}/${LIB_DIR})
 	SET(Simox_INSTALL_BIN_DIR ${CMAKE_INSTALL_PREFIX}/${BIN_DIR})
 	SET(Simox_INSTALL_HEADER_DIR ${CMAKE_INSTALL_PREFIX}/include)
-	
-	
+
+
 	## set library settings
-	
-	    ## SETUP VIRTUAL ROBOT 
+
+	    ## SETUP VIRTUAL ROBOT
     	SET(VirtualRobot_BUILD_DIRECTORY ${Simox_BUILD_DIRECTORY})
     	SET(VirtualRobot_INSTALL_LIB_DIR ${Simox_INSTALL_LIB_DIR})
         SET(VirtualRobot_INSTALL_BIN_DIR ${Simox_INSTALL_BIN_DIR})
@@ -48,16 +48,17 @@ IF (NOT Simox_CONFIGURED)
         # set SIMOX vars according to VirtualRobot config
        	SET(Simox_VISUALIZATION ${VirtualRobot_VISUALIZATION})
 		SET(Simox_USE_COIN_VISUALIZATION ${VirtualRobot_USE_COIN_VISUALIZATION})
-		SET(Simox_USE_OPENSCENEGRAPH_VISUALIZATION ${VirtualRobot_USE_OPENSCENEGRAPH_VISUALIZATION})
+        SET(Simox_USE_OPENSCENEGRAPH_VISUALIZATION ${VirtualRobot_USE_OPENSCENEGRAPH_VISUALIZATION})
+        SET(Simox_USE_COLLADA ${VirtualRobot_USE_COLLADA})
 		SET(Simox_VISUALIZATION_LIBS ${VirtualRobot_VISUALIZATION_LIBS})
 		SET(Simox_VISUALIZATION_INCLUDE_PATHS ${VirtualRobot_VISUALIZATION_INCLUDE_PATHS})
 		SET(Simox_VISUALIZATION_COMPILE_FLAGS ${VirtualRobot_VISUALIZATION_COMPILE_FLAGS})
-        
+
         ## SETUP SABA
         # nothing to do yet
 
         ## SETUP GRASP STUDIO
         # nothing to do yet
 
-     
+
 ENDIF(NOT Simox_CONFIGURED)
